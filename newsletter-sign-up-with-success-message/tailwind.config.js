393990fc6588883680fb589f1166e38dacb929d0
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2024-03-11 14:39:44 +0100

    Cambios

--- newsletter-sign-up-with-success-message/tailwind.config.js
+++ newsletter-sign-up-with-success-message/tailwind.config.js
@@ -15,7 +15,12 @@ export default {
         'dark-slate-grey': '#242742',
         'charcoal-grey': '#36384E',
         'grey': '#9294A0',
+        'wild-watermelon': '#FF527B',
+        'burning-orange': '#FF6A3A',
       },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 1)',
+      }
     },
   },
   plugins: [],
